2ead04e0eecc70fb4befba1411d27d5425636d9a
kostasrazgelis <50516150+kostasrazgelis@users.noreply.github.com>
2020-12-23 19:10:29 +0200

    Version 0.3

--- automata_theory_excercise_2.py
+++ automata_theory_excercise_2.py
@@ -1,52 +1,79 @@
 import sys, getopt
 from numpy import *
 
+
 class Grammar:
-    def __init__(self):
+    def __init__(self, file):
+        self.total_conditions = []
         self.non_ending_conditions = []
         self.ending_conditions = []
-        self.syntax = []
+        self.automate_stack = []
+        self.alphabet = []
+        self.file = file
+        self.matrix = 0
+
+    def initialize_grammar(self):
+        tmp = self.file.read().split()
+
+        K = int(tmp[0])
+        for x in range(1, 1 + K):
+            self.non_ending_conditions.append(tmp[x])
+            self.total_conditions.append(tmp[x])
+
+        L = int(tmp[K + 1])
+        for x in range(K + 1 + L, K + 2 + L):
+            self.ending_conditions.append(tmp[x])
+            self.total_conditions.append(tmp[x])
+
+        N = int(tmp[K + L + 2])
+
+        col = 2
+        rows = N
+        step = K + L + N
+
+        for j in range(0, rows):
+            for i in range(0, col):
+                for x in range(len(tmp[step + 2 * j + i])):
+                    if tmp[(step + 2 * j + i)][x] not in self.alphabet:
+                        self.alphabet.append(str(tmp[(step + 2 * j + i)][x]))
+
+        self.matrix = [[0 for x in range(col)] for y in range(rows + len(self.alphabet))]
+
+        for j in range(0, rows):
+            for i in range(0, col):
+                self.matrix[j][i] = tmp[(step + 2 * j + i)]
+
+        j = len(self.matrix) - len(self.alphabet)
+        for letter in self.alphabet:
+            if str(letter) != 'S':
+                self.matrix[j][0] = str(letter)
+                self.matrix[j][1] = str(letter)
+                j += 1
+
+    def print_grammar(self):
+        print("M = (K ,Σ ,Γ , Δ , s , F)")
+        print("  - K =" + str(self.total_conditions))
+        print("  - Σ =" + str(self.alphabet))
+        print("  - Γ =")
+        print("  - s =" + str(self.non_ending_conditions))
+        print("  - F =" + str(self.ending_conditions))
+        print("  - D = ")
+        print(*self.matrix, sep='\n')
 
 
-
-
-def initialize_grammar(G):
+def initialize_grammar():
     try:
+        print("The file is scanned... ")
         inputfile = open(sys.argv[1], 'r')
+        g = Grammar(inputfile)
+        g.initialize_grammar()
+
+        g.print_grammar()
     except:
         print("file is not correct and did not open.... please put a correct root")
         sys.exit(1)
     else:
-        print('The file has been opened, scanning the grammar now')
-
-    tmp = inputfile.read().split()
-
-    K = int(tmp[0])
-    for x in range(1, 1+K):
-        G.non_ending_conditions.append(tmp[x])
-
-    L = int(tmp[K+1])
-    for x in range(K+1+L, K+2+L):
-        G.ending_conditions.append(tmp[x])
-
-    N = int(tmp[K+L+2])
-
-    matrix = []
-    list = []
-
-
-    for x in range(K+L+2, K+L+N+3,2):
-        list.append(tmp[x])
-        list.append(tmp[x+1])
-        matrix.append(list)
-
-
-    list.append(0)
-    print(matrix)
-
-
-    print(G.non_ending_conditions)
-    print(G.ending_conditions)
+        print('The grammar has been created correctly...')
 
 
 def main(argv):
@@ -60,11 +87,8 @@ def main(argv):
         print("Not the correct format.... python test.py <file.txt>")
         sys.exit(1)
 
-    G = Grammar()
-
-    initialize_grammar(G)
+    initialize_grammar()
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
